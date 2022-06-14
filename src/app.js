5386bcf40542affaa3e64ac72a1cf0b8b4a603d2
WerkaBur <werkabur@gmail.com>
2022-06-14 17:51:49 +0200

    Update app.js
    Updated search engine

--- src/app.js
+++ src/app.js
@@ -47,5 +47,19 @@ let apiKey = `79a3607cf44d8fac8c27b9322d439b62`;
 let cityName = `Monaco`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
